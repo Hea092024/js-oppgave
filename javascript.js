e41e6dbd528fb261544a24dfcb1da09000da8ff9
Hea092024 <Hea092024@gmail.com>
2024-10-17 17:37:13 +0200

    add comments

--- javascript.js
+++ javascript.js
@@ -52,11 +52,12 @@ Try to use both the let and const keywords
 
 ******************************************************************************/
 
-// const greetings = "hello, world"; //string = tekst
-// const num1 = 1+1; //number = tall
-// const isThisGuy = true; //boolean = true/false
-// const shoppinglist = ["tomat", "egg","brød"]; // Array = liste
-// console.log(greetings, num1, isThisGuy, shoppinglist);
+const greetings = "hello, world"; //string = tekst
+const num1 = 1+1; //number = tall
+const isThisGuy = true; //boolean = true/false
+const shoppinglist = ["tomat", "egg","brød"]; // Array = liste
+console.log(greetings, num1, isThisGuy, shoppinglist);
+
 /****************************************************************************** 
 
 ASSIGNMENT 3
@@ -75,9 +76,8 @@ const numb3 = 5/5;
 const numb4 = 5*5
 console.log(numb1, numb2, numb3, numb4);
 
-
 let num = 5 //verdien er 5
-num+=1 // pluss 1
+num++ // pluss 1
 num++ // pluss 1
 num++ // pluss 1
 console.log(num); // svaret er 8
@@ -85,19 +85,19 @@ num++ // pluss 1
 num++ // pluss 1
 console.log(num); //svaret er 10
 
-
 let numero = 5 // Verdien er 5
 numero--  // minus 1
 console.log(numero) // = 4
 
-
 let numm = 30 //verdien er 30
 let numm2 = 20 // verdien er 20
-numm += numm2 // 30+20+20 
+numm += numm2 // 30+20+20
 console.log(numm + numm2) // 50 + 20 = 70
 
-
-
+let tall = 30 // verdien er 30
+let tall2 =20 // verdien er 20
+tall -= tall2  // 30-20-20
+console.log(tall - tall2); // 10 - 20 = -10
 
 /****************************************************************************** 
 
@@ -133,22 +133,30 @@ can handle all cases correctly
 
 ******************************************************************************/
 
-let userName = "";
+let userName = "hector"; // en string som holder på userName som er da "hector"
 
-let userAge = 18;
+let userAge = 18; // ett nummer som viser alderen til userAge
 
-let userIsLoggedIn = false;
+let userIsLoggedIn = false; //en boolean som sjekker om userIsLoggedIn er false eller true ( i dette tilfellet så er den false)
 
-let userIsBlocked = false;
+let userIsBlocked = false;// enda en boolean som sjekker om userIsBlocked er false eller true ( i dette tilfellet så er den false)
 
-let goToPage = "";
+let goToPage = ""; // en string som sender bruker til neste "page"
 
-//your code here
+if (userName && userAge >= 18 && !userIsBlocked) {
+  userIsLoggedIn = true; 
+  console.log("Welcome");
+  goToPage = "/home";
+} else {
+  console.log("error");
+}
+//koden sjekker om userName er tom, om userAge er eldre enn 18 og om userIsBlocked er "blocked"
+
+//Hvis alt stemmer så vil konsolen vise "Welcome" , hadde noe feilet så hadde man fått "error" meldingen
 
 /******************************************************************************
-
+ 
 ASSIGNMENT 5
-
 
 
 Make a variable called userTitle and set the content equal to "Mr." if userMale 
@@ -158,10 +166,7 @@ is true, or to "Mrs." if userMale is false. Use the TERNARY conditional to do
 this:
 
 
-
 const variable = statement ? "this if true" : "this if not true"
-
-
 
 Try changing userMale to both true and false and console.log your new variable,
 
@@ -169,6 +174,10 @@ to see that your conditional is working.
 
 ******************************************************************************/
 
-const userMale = false;
+let userMale = true; 
+let userTitle = userMale ? 'Mr.' : 'Mrs.';
+console.log(userTitle);
 
-//your code here
+// userMale sin verdi er "true"
+// ? sjekker om userTitle eller userMale er "true" eller "false"
+// : gir den verdien om den er true eller false ( venstre = true ) ( høyre = false)
